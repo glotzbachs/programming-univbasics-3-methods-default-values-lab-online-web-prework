09a131a3560306573f70bd376438e4a60fd35708
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-09-16 16:45:19 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,6 +3,6 @@ def meal_choice(veg1="broccoli",veg2="macaroni",protein="meat")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
-meal_choice=String
+
 
 
